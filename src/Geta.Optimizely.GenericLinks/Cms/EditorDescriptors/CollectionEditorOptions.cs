2315aa0312db17d4b6a1f0d5a334a439ace15a59
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2022-04-12 15:51:29 +0300

    Added license header

--- src/Geta.Optimizely.GenericLinks/Cms/EditorDescriptors/CollectionEditorOptions.cs
+++ src/Geta.Optimizely.GenericLinks/Cms/EditorDescriptors/CollectionEditorOptions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
 namespace Geta.Optimizely.GenericLinks.Cms.EditorDescriptors
 {
